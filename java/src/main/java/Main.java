6abd4e638ccabd4bacc9cdd34f04a3db95b9f725
Nick Ng <ngyewch@gmail.com>
2023-12-23 15:49:42 +0800

    feat: updated Java implementation

--- java/src/main/java/Main.java
+++ java/src/main/java/Main.java
@@ -3,6 +3,8 @@ import io.helidon.webserver.WebServer;
 import rpc.AddRequest;
 import rpc.AddResponse;
 import rpc.RpcTwirp;
+import rpc2.DoSomethingRequest;
+import rpc2.DoSomethingResponse;
 
 public class Main {
   public static void main(String[] args) throws Exception {
@@ -13,7 +15,10 @@ public class Main {
                 Routing.builder()
                     .register(
                         "/twirp",
-                        rules -> RpcTwirp.Helidon.TestService.update(rules, new TestService()))
+                        rules -> {
+                          RpcTwirp.Helidon.TestService.update(rules, new TestService());
+                          RpcTwirp.Helidon.TestService2.update(rules, new TestService2());
+                        })
                     .build())
             .build();
     webServer.start();
@@ -24,6 +29,14 @@ public class Main {
     @Override
     public AddResponse add(AddRequest input) {
       return AddResponse.newBuilder().setValue(input.getA() + input.getB()).build();
+    }
+  }
+
+  public static class TestService2 implements RpcTwirp.TestService2 {
+
+    @Override
+    public DoSomethingResponse doSomething(DoSomethingRequest input) {
+      return DoSomethingResponse.newBuilder().setText(input.getText().toUpperCase()).build();
     }
   }
 }
